ff87a6d4507b913db1c5d4350e8596f7ce4b872e
Dominick Hem <domhem@csu.fullerton.edu>
2018-03-19 02:37:26 -0700

    Added sidebar menus to edit info

--- scripts/profile_main.js
+++ scripts/profile_main.js
@@ -1,6 +1,7 @@
 (function(window) {
   "use strict";
-  var FORM_SELECTOR = "[data-profile=\"form\"]";
+  var PROFILE_FORM_SELECTOR = "[data-profile=\"form\"]";
+  var SM_FORM_SELECTOR = "[data-social-media=\"form\"]";
 
   var App = window.App || {};
   var Profile = App.Profile;
@@ -8,13 +9,19 @@
   var FormHandler = App.FormHandler;
   var myProfile = new Profile("ncc-1701", new DataStore());
   window.myProfile = myProfile;
-  var formHandler = new FormHandler(FORM_SELECTOR);
+  var profileHandler = new FormHandler(PROFILE_FORM_SELECTOR);
+  var smHandler = new FormHandler(SM_FORM_SELECTOR);
 
-  formHandler.addSubmitHandler(function (data) {
+  profileHandler.addSubmitHandler(function (data) {
     myProfile.createProfile.call(myProfile, data);
     myProfile.editName.call(myProfile, data);
     myProfile.editLogo.call(myProfile, data);
     myProfile.editPhone.call(myProfile, data);
     myProfile.editEmail.call(myProfile, data);
   });
+
+  smHandler.addSubmitHandler(function (data) {
+    
+  });
+
 })(window);
--- scripts/profile_sidebar.js
+++ scripts/profile_sidebar.js
@@ -1,12 +1,34 @@
-function openRightMenu() {
-  document.getElementById("main").style.marginRight = "25%";
-  document.getElementById("rightMenu").style.width = "25%";
-  document.getElementById("rightMenu").style.display = "block";
+function openOptionMenu() {
+  document.getElementById("main").style.marginRight = "30%";
+  document.getElementById("optionMenu").style.width = "30%";
+  document.getElementById("optionMenu").style.display = "block";
   document.getElementById("nav").style.display = "none";
 }
 
-function closeRightMenu() {
+function closeOptionMenu() {
   document.getElementById("main").style.marginRight = "0%";
-  document.getElementById("rightMenu").style.display = "none";
+  document.getElementById("optionMenu").style.display = "none";
   document.getElementById("nav").style.display = "inline-block";
+}
+
+function openEditMenu() {
+  document.getElementById("main").style.marginRight = "30%";
+  document.getElementById("editMenu").style.width = "30%";
+  document.getElementById("editMenu").style.display = "block";
+  document.getElementById("nav").style.display = "none";
+}
+
+function closeEditMenu() {
+  document.getElementById("editMenu").style.display = "none";
+}
+
+function openSMMenu() {
+  document.getElementById("main").style.marginRight = "30%";
+  document.getElementById("smMenu").style.width = "30%";
+  document.getElementById("smMenu").style.display = "block";
+  document.getElementById("nav").style.display = "none";
+}
+
+function closeSMMenu() {
+  document.getElementById("smMenu").style.display = "none";
 }
