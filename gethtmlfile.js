5b0bfcbbe3f286b3811d168678828046e4ecabb8
Daniel Bravo <bravod@csu.fullerton.edu>
2018-03-25 13:10:23 -0700

    Add redirect after login

--- gethtmlfile.js
+++ gethtmlfile.js
@@ -14,6 +14,7 @@ pages = {
   "/logout" : "logout.html",
   "/profile": "profile.html",
   "/signup" : "signup.html",
+  "/welcome" : "welcome-page.html",
   "404" : "404.html",
 };
 
--- scripts/login_remotedatastore.js
+++ scripts/login_remotedatastore.js
@@ -12,13 +12,37 @@
 
     this.serverUrl = url;
   }
-  
+
+  RemoteDataStore.prototype.query = function (d, cb) {
+    $.ajax(this.serverUrl, {
+      type: "GET",
+      data: d
+    }).done(function(serverResponse) {
+      console.log(serverResponse);
+      cb(serverResponse);
+    });
+  };
+
   RemoteDataStore.prototype.login = function (d, cb) {
     $.ajax(LOGIN_URL, {
       type: "Post",
       data: d
     }).done(function(serverResponse) {
       console.log(serverResponse);
+      var a = {
+        sid: serverResponse.id
+      };
+
+      $.ajax("http://localhost:2403/users", {
+        type: "GET",
+        data: a
+      }).done(function(serverResponse) {
+        console.log("get me", serverResponse);
+        window.location = "/profile/" + serverResponse[0].displayname;
+        cb(serverResponse);
+      });
+
+
       cb(serverResponse);
     });
 };
